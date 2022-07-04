630256518cfaf6ec9420b87fdc942c4121588bc7
Luna Orlata <the.keksik.is.a.lie@gmail.com>
2022-07-04 15:48:05 +0300

    added camera player follow. changed tank movement to use velocity.

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -5,20 +5,14 @@ public class TankController : MonoBehaviour
     public float speed;
 
     private Rigidbody rb;
-    private Vector3 moveDirection = Vector3.zero;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    private void FixedUpdate()
-    {
-        rb.MovePosition(transform.position + moveDirection * speed * Time.fixedDeltaTime);
-    }
-
     public void SetMovementDirection(Vector3 direction)
     {
-        moveDirection = direction;
+        rb.velocity = direction * speed;
     }
 }
